d1cc47214e0cca1c08ee1a23bc2538de85821780
Abakrii <abdelrahmansalembakry@gmail.com>
2020-01-17 16:22:25 +0400

    adding toolbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import style from "./Layout.module.css";
-
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 const Layout = ({ children }) => {
   return (
     <div>
+      <Toolbar/>
       <div>Toolbar, SideDrawe, BackDrop</div>
       <main className={style.Content}>{children}</main>
     </div>
---
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Toolbar.module.css';
+const Toolbar = () => (
+    <header className={style.Toolbar}>
+        <div >
+            Menu            
+        </div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+)
+
+export default Toolbar;
