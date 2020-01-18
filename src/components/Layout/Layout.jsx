83bb9d6347bef1cb1e4419b7821b5c106c6f3f2f
Abakrii <abdelrahmansalembakry@gmail.com>
2020-01-18 04:35:50 +0400

    Create Responsive Side Drawer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import style from "./Layout.module.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SildeDrawer/SideDrawer'
 const Layout = ({ children }) => {
   return (
     <div>
       <Toolbar/>
+      <SideDrawer/>
       <div>Toolbar, SideDrawe, BackDrop</div>
       <main className={style.Content}>{children}</main>
     </div>
---
+++ src/components/Navigation/SildeDrawer/SideDrawer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import style from './SideDrawer.module.css';
+
+const SideDrawer = (props) => {
+
+    return (
+        <div className={style.SideDrawer}>
+
+            
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    );
+}
+
+
+export default SideDrawer
